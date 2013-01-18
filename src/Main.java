ab86cbc022fc0773efa263971425a95c2ab672ff
Stefan Yip <stefanyip@hotmail.com>
2013-01-18 11:10:58 +0100

    Initial

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Simulator s = new Simulator(5, 5);
+	}
+
+}
